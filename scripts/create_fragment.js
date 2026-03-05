b85dd0f468f5b99895b2238b49abc4e584b7d24d
Ajoy Bhunia <ajoybhunia@Ajoys-MacBook-Pro.local>
2026-03-05 09:44:24 +0530

    separates create an element from create_fragment

--- scripts/create_fragment.js
+++ scripts/create_fragment.js
@@ -1,9 +1,15 @@
-export const createFragment = ([tag, attrs, children]) => {
+const createAnElement = (tag, attrs) => {
   const element = document.createElement(tag);
 
   Object.entries(attrs).forEach(([key, value]) => {
     element.setAttribute(key, value);
   });
+
+  return element;
+};
+
+export const createFragment = ([tag, attrs, children]) => {
+  const element = createAnElement(tag, attrs);
 
   children.forEach((child) => {
     element.append(
